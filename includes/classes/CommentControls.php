b8f6321afddd7294821948333e685f0070f35b05
TehStud <igortadic.msc@gmail.com>
2022-07-04 21:08:28 +0200

    creating comment reply section

--- includes/classes/CommentControls.php
+++ includes/classes/CommentControls.php
@@ -18,10 +18,13 @@ class VideoInfoControls {
     $dislikeBUtton = $this->createDislikeButton();
     $replySection = $this->createReplySection();
 
-    return "<div class='controls>
+    return "<div class='controls'>
+      $replyButton
+      $likesCount
       $likeButton
       $dislikeBUtton
-    </div>'"
+    </div>
+    $replySection";
   }
 
   private function createReplyButton() {
@@ -43,7 +46,24 @@ class VideoInfoControls {
   }
 
   private function createReplySection() {
-    return "";
+    $postedBy = $this->userLoggedInObj->getUsername();
+    $videoId = $this->comment->getVideoId();
+    $commentId = $this->comment->getId();
+
+    $profileButton = ButtonProvider::createUserProfileButton($this->$con, $postedBy);
+
+    $cancelButtonAction = "toggleReply(this)";
+    $cancelButton = ButtonProvider::createButton("Cancel", null, $commentAction, "cancelComment");
+
+    $postButtonAction = "postComment(this, \"$postedBy\", $videoId, $commentId, \"repliesSection\")";
+    $postButton = ButtonProvider::createButton("Reply", null, $postButtonAction, "postComment");
+
+    return "<div class='commentForm hidden'>
+        $profileButton
+        <textarea class='commentBodyClass' placeholder='Add a comment'></textarea>
+        $cancelButton
+        $postButton
+      </div>";
   }
 
   private function createLikeButton() {
